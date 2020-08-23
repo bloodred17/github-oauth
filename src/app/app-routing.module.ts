014b129b07056bcfbd06bde099dbc86fdbfdc5ca
Ankur Dutta <ankur.611@gmail.com>
2020-08-24 01:46:05 +0530

    auth handler added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthHandlerComponent } from './auth-handler/auth-handler.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', component: AuthHandlerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthHandlerComponent } from './auth-handler/auth-handler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthHandlerComponent
   ],
   imports: [
     BrowserModule,
