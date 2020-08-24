5a15a22e1ecbc487e7272b86b7976aa3563b08ef
Ankur Dutta <ankur.611@gmail.com>
2020-08-24 05:57:23 +0530

    update

--- src/app/auth-handler/auth-handler.service.ts
+++ src/app/auth-handler/auth-handler.service.ts
@@ -12,10 +12,12 @@ export class AuthHandlerService {
 
   auth() {
     return this.http.post('https://github.com/login/oauth/access_token', {
-      client_id: '3da366587e2577cb7d0f',
-      redirect_uri: 'https://github-oauth.netlify.app/auth',
-      client_secret: '46238833d956a3705e61c2e6f15c19bf474e4d2d',
-      code: '061d3283c2bd1381a8fd'
+      params: {
+        client_id: '3da366587e2577cb7d0f',
+        redirect_uri: 'https://github-oauth.netlify.app/auth',
+        client_secret: '46238833d956a3705e61c2e6f15c19bf474e4d2d',
+        code: '061d3283c2bd1381a8fd'
+      }
     }).subscribe((data) => {
       console.log(data);
     });
